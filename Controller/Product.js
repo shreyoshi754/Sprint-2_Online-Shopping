33dfdbe5ca1b367427178fb8cadf8d8f3e896620
shreyoshi754 <67384790+shreyoshi754@users.noreply.github.com>
2022-05-16 13:26:01 +0530

    product added

--- Controller/Product.js
+++ Controller/Product.js
@@ -1,8 +1,33 @@
-
-
+const prisma = require('../Db/index')
 exports.addProduct=async(req, res, next) => {
   const { name, price, catagory } = req.body;
   console.log(req.body)
-  res.send('Product added')
-
+  try {
+    const product = await prisma.product.create({
+        data:{name,price,catagory}
+    });
+    res.send(product);
+    console.log("Product added");
+} catch (error) {
+    const errorToThrow = new Error();
+    switch (error?.code) {
+        case '23505':
+            errorToThrow.message = 'Product already exists';
+            errorToThrow.statusCode = 403;
+            break;
+        default:
+            errorToThrow.statusCode = 500;
+    }
+    next(errorToThrow);
 }
+};
+exports.viewProduct = async (req, res, next) => {
+  let {name, price, catagory} = req.body;
+   try{
+      const allProducts = await prisma.user.findMany();
+  res.send(allProducts);
+}
+catch(err){
+  console.log(err)
+}
+};
--- Routes/Product.js
+++ Routes/Product.js
@@ -2,4 +2,5 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../Controller/Product');
 router.post('/add',productController.addProduct);
+router.get('/view',productController.viewProduct);
 module.exports = router;
