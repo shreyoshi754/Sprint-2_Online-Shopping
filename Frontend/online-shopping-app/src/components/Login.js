6d9bf869f23e69ddb32f0c58669df20af69d66f2
shreyoshi754 <67384790+shreyoshi754@users.noreply.github.com>
2022-05-18 17:35:28 +0530

    login and register added

---
+++ Frontend/online-shopping-app/src/components/Login.js
@@ -0,0 +1,109 @@
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import {fa-regular,fa-circle-user} from '@fortawesome/free-solid-svg-icons'
+import './Form.css';
+import { useState } from 'react';
+
+export default function Form() {
+
+	// States for registration
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	// States for checking the errors
+	const [submitted, setSubmitted] = useState(false);
+	const [error, setError] = useState(false);
+
+	// Handling the name change
+	const handleName = (e) => {
+		setName(e.target.value);
+		setSubmitted(false);
+	};
+
+	// Handling the email change
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+		setSubmitted(false);
+	};
+
+	// Handling the password change
+	const handlePassword = (e) => {
+		setPassword(e.target.value);
+		setSubmitted(false);
+	};
+
+	// Handling the form submission
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (name === '' || email === '' || password === '') {
+			setError(true);
+		} else {
+			setSubmitted(true);
+			setError(false);
+		}
+	};
+
+	// Showing success message
+	const successMessage = () => {
+		return (
+			<div
+				className="success"
+				style={{
+					display: submitted ? '' : 'none',
+				}}>
+				<h1>User {name} successfully registered!!</h1>
+			</div>
+		);
+	};
+
+	// Showing error message if error is true
+	const errorMessage = () => {
+		return (
+			<div
+				className="error"
+				style={{
+					display: error ? '' : 'none',
+				}}>
+				<h1>Please enter all the fields</h1>
+			</div>
+		);
+	};
+
+	return (
+		<div className="container">
+			<div className="form-box">
+				<div className="header-form">
+					<h4 className="text-primary text-center"><i class="fa-solid fa-circle-user" style={{ fontSize: "110px" }}></i></h4>
+					<div className="image">
+					</div>
+				</div>
+				<div className="body-form">
+					<form>
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span className="input-group-text"><i class="fa fa-user"></i></span>
+							</div>
+							<input type="text" className="form-control" placeholder="Email" />
+						</div>
+						<div className="input-group mb-3">
+							<div className="input-group-prepend">
+								<span className="input-group-text"><i class="fa fa-lock"></i></span>
+							</div>
+							<input type="text" className="form-control" placeholder="Password" />
+						</div>
+						<button type="button" className="btn btn-secondary btn-block">LOGIN</button>
+						<div className="message">
+							<div><input type="checkbox" /> Remember ME</div>
+							<div><a href="#">Forgot your password</a></div>
+						</div>
+					</form>
+					<div className="social">
+						<a href="#"><i className="fab fa-facebook"></i></a>
+						<a href="#"><i className="fab fa-twitter-square"></i></a>
+						<a href="#"><i className="fab fa-google"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
