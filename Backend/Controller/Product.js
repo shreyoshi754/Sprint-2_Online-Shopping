55025e28e5778d1db2429cc94104cbddc031f552
souvik-xavi <souvikghata2@gmail.com>
2022-05-21 10:50:15 +0530

    backend change

--- Backend/Controller/Product.js
+++ Backend/Controller/Product.js
@@ -1,11 +1,11 @@
 const prisma = require('../Db/index')
 exports.addProduct = async (req, res, next) => {
-  const { name, price, catagory } = req.body;
+  const { name, price, catagory,url } = req.body;
   console.log(req.user);
   try {
     if (req.user.role === 'seller') {
       const product = await prisma.product.create({
-        data: { name, price, catagory }
+        data: { name, price, catagory,url }
       });
       console.log("Product added");
       return res.send(product);
